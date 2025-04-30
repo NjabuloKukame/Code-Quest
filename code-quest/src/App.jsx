0673e6549be84a850acb829490a1e437c72407b0
NKukame <NKukame@jhb.dvt.co.za>
2025-04-30 13:18:59 +0200

    Add Header component with styling and SVG assets
    - Created Header component with navigation links and dark mode toggle.
    - Added CSS styles for header layout and logo.
    - Included React logo SVG asset for branding.
    - Set up global styles and font imports in index.css.
    - Configured Vite for React application.

---
+++ code-quest/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from './components/Header'
+import ligtBackgroundImage from "./assets/code-quest-light-mode.jpg";
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="app-body">
+      <img src={ligtBackgroundImage} alt="Light Mode Background Image" className='background-image'/>
+      
+    </div>
+    </>
+  )
+}
+
+export default App
