16f368b7bc4926710a1bd6b4a8ba2c7a8a164653
NjabuloKukame <njabulokukame60@gmail.com>
2025-05-01 16:26:11 +0200

    Implement dark mode feature with context provider and update styles

--- code-quest/src/App.jsx
+++ code-quest/src/App.jsx
@@ -1,18 +1,30 @@
-import Header from './components/Header'
-import ligtBackgroundImage from "./assets/code-quest-light-mode.jpg";
-import './App.css'
+import { useDarkMode } from "./components/DarkMode";
+import Header from "./components/Header";
+import lightBackgroundImage from "./assets/code-quest-light-mode.jpg";
+import darkBackgroundImage from "./assets/code-quest-dark-mode.jpg";
+import "./App.css";
 
 function App() {
-  
-
+  const { darkMode } = useDarkMode();
   return (
     <>
-    <div className="app-body">
-      <img src={ligtBackgroundImage} alt="Light Mode Background Image" className='background-image'/>
-      
-    </div>
+      <div className="app-body">
+        <img
+          src={darkMode ? darkBackgroundImage : lightBackgroundImage} 
+          alt={
+            darkMode
+              ? "Dark Mode Background Image"
+              : "Light Mode Background Image"
+          }
+          className="background-image"
+        />
+
+        <div className="header-container">
+          <Header />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
